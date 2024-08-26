f0b9f5e32f082f0aa61154a7bd0d4339f0b8c4a9
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-08-26 03:47:01 +0200

    update

--- configuration.nix
+++ configuration.nix
@@ -120,6 +120,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
 	git
+  kitty
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
