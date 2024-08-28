6435ae25fbf187efdd42188500e86038f6e42784
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-08-28 08:08:57 +0200

    update

--- hosts/voyager/modules/system/services.nix
+++ hosts/voyager/modules/system/services.nix
@@ -1,18 +1,10 @@
 { ... }:
 {
-  services = {
-    # Enable CUPS to print documents.
-    printing.enable = true;
-
-    displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-      };
-    };
-  };
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
   # services.displayManager.autoLogin.enable = true;
   # services.displayManager.autoLogin.user = "simon";
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
+  
 }
