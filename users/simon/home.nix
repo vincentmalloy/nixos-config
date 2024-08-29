b0ee29cd473dd776f2d771129e7f84490319cede
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-08-29 12:30:05 +0200

    update

--- users/simon/home.nix
+++ users/simon/home.nix
@@ -1,75 +1,93 @@
-{ lib, config, pkgs, inputs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
-	
-	home = {
-		username = lib.mkDefault "simon";
-		homeDirectory = lib.mkDefault "/home/${config.home.username}";
-	};
 
-	programs.git = {
-		enable = true;
-		userName = "Simon Lundius";
-		userEmail = "25029432+vincentmalloy@users.noreply.github.com";
-	};
+  home = {
+    username = lib.mkDefault "simon";
+    homeDirectory = lib.mkDefault "/home/${config.home.username}";
+  };
 
-	programs.zsh = {
-		enable = true;
-		autosuggestion.enable = true;
-		syntaxHighlighting.enable = true;
-		ohMyZsh = {
-			enable = true;
-			theme = "agnoster";
-			plugins = [
-				"git"
-				"history"
-				"wd"
-			];
-		};
-	};
+  programs.git = {
+    enable = true;
+    userName = "Simon Lundius";
+    userEmail = "25029432+vincentmalloy@users.noreply.github.com";
+  };
 
-	programs.helix = {
-		enable = true;
-		defaultEditor = true;
-		settings = {
-			theme = "voyager";
-			editor = {
-				true-color=true;
-				bufferline="multiple";
-				cursorline=true;
-				rulers=[120];
-				cursor-shape = {
-					insert = "bar";
-					normal = "block";
-					select = "underline";
-				};
-				indent-guides = {
-					render = true;
-				};
-			};
-		};
-		themes = {
-			voyager = {
-				inherits = "molokai";
-				"ui.background" = {};
-			};
-		};
-	};
+  programs.eza = {
+    enable = true;
+    icons = true;
+    extraOptions = [
+      "--group-directories-first"
+    ];
+  };
 
-	programs.kitty = {
-		enable = true;
-		font = {
-			name = "CommitMono Nerd Font";
-			size = 11;
-		};
-		theme = "Monokai Soda";
-		settings = {
-			background_opacity = "0.5";
-			background = "#000000";
-		};
-	};
+  programs.zsh = {
+    enable = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+    shellAliases = {
+      nup = "sudo nixos-rebuild switch --upgrade --recreate-lock-file --flake ${config.home.homeDirectory}/nixos-config";
+      nre = "sudo nixos-rebuild switch --flake ${config.home.homeDirectory}/nixos-config";
+    };
+    oh-my-zsh = {
+      enable = true;
+      theme = "agnoster";
+      plugins = [
+        "git"
+        "history"
+        "wd"
+      ];
+    };
+  };
 
-	programs.firefox = {
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "voyager";
+      editor = {
+        true-color = true;
+        bufferline = "multiple";
+        cursorline = true;
+        rulers = [ 120 ];
+        cursor-shape = {
+          insert = "bar";
+          normal = "block";
+          select = "underline";
+        };
+        indent-guides = {
+          render = true;
+        };
+      };
+    };
+    themes = {
+      voyager = {
+        inherits = "molokai";
+        "ui.background" = { };
+      };
+    };
+  };
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "CommitMono Nerd Font";
+      size = 11;
+    };
+    theme = "Monokai Soda";
+    settings = {
+      background_opacity = "0.5";
+      background = "#000000";
+    };
+  };
+
+  programs.firefox = {
     enable = true;
 
     # profiles.simon = {
@@ -83,27 +101,27 @@
     #         view-image
     #         ublock-origin
     #         youtube-shorts-block
-				# 		sponsor-block
+    # 		sponsor-block
     #     ];
     # };
   };
 
-	gtk = {
-		enable = true;
-		gtk3.extraConfig = {
-			settings = ''
-				gtk-application-prefer-dark-theme = 1;
-			'';
-		};
-		gtk4.extraConfig = {
-			settings = ''
-				gtk-application-prefer-dark-theme = 1;
-			'';
-		};
-		
-	};
-	
-	home.packages = with pkgs; [
-		helix
-	];
+  gtk = {
+    enable = true;
+    gtk3.extraConfig = {
+      settings = ''
+        	gtk-application-prefer-dark-theme = 1;
+      '';
+    };
+    gtk4.extraConfig = {
+      settings = ''
+        	gtk-application-prefer-dark-theme = 1;
+      '';
+    };
+
+  };
+
+  home.packages = with pkgs; [
+    helix
+  ];
 }
