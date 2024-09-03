c6222fe5fca8d1a313e0e62965ab0c31e5b0f63e
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-09-03 09:57:51 +0200

    update

--- hosts/voyager/modules/gui/displaymanager.nix
+++ hosts/voyager/modules/gui/displaymanager.nix
@@ -1,10 +1,18 @@
 { pkgs, ... }:
+let
+  # tokyo-night-sddm = pkgs.libsForQt5.callPackage ./tokyo-night-sddm.nix { inherit pkgs; };
+  sddm-astronaut-theme = pkgs.libsForQt5.callPackage ./sddm-astronaut-theme.nix { inherit pkgs; };
+in
 {
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
-    theme = "${import ./tokyo-night-sddm.nix { inherit pkgs; }}";
+    theme = "sddm-astronaut-theme"; #elarun, maldives, maya
   };
+  environment.systemPackages = [
+    # pkgs.libsForQt5.qtgraphicaleffects
+    sddm-astronaut-theme
+  ];
   # services = {
   #   xserver = {
   #     enable = true;
--- hosts/voyager/modules/gui/tokyo-night-sddm.nix
+++ hosts/voyager/modules/gui/tokyo-night-sddm.nix
@@ -1,18 +1,20 @@
 { pkgs }:
-let
-  image = ../../../../../images/desktop/desktop_left.jpg;
-in
-pkgs.stdenv.mkDerivation {
-  name = "tokyo-night-sddm.nix";
-  src = pkgs.fetchFromGithub {
+# let
+#   image = ../../../../../images/desktop/desktop_left.jpg;
+# in
+pkgs.stdenv.mkDerivation rec {
+  name = "tokyo-night-sddm";
+  version = "320c8e74";
+  src = pkgs.fetchFromGitHub {
     owner = "siddrs";
     repo = "tokyo-night-sddm";
     rev = "320c8e74ade1e94f640708eee0b9a75a395697c6";
     sha256 = "JRVVzyefqR2L3UrEK2iWyhUKfPMUNUnfRZmwdz05wL0=";
   };
+  dontBuild = true;
   installPhase = /* sh */ ''
     mkdir -p $out/share/sddm/themes
-    cp -AR $src $out/share/sddm/themes/tokyo-night-sddm
-    cp ${image} $out/share/sddm/themes/tokyo-night-sddm/Backgrounds/voyager.jpg
+    cp -aR $src $out/share/sddm/themes/tokyo-night-sddm
   '';
+    # cp ${image} $out/share/sddm/themes/tokyo-night-sddm/Backgrounds/voyager.jpg
 }
