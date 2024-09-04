711fe8a7b7314aa7e40122271d080349260b987f
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-09-04 19:51:58 +0200

    update

--- update.zsh
+++ update.zsh
@@ -3,12 +3,11 @@
 nix-prefetch-git --url git@github.com:4b85151d28b3025451f5634b6081019d > tmp-omp-json
 url=$(cat ./tmp-omp-json | jq .url)
 rev=$(cat ./tmp-omp-json | jq .rev)
-sha256=$(cat ./tmp-omp-json | jq .sha256)
+# sha256=$(cat ./tmp-omp-json | jq .sha256)
 cat <<eos > users/simon/fetch-omp-config.nix
 {
   url = $url;
   rev = $rev;
-  sha256 = $sha256;
 }
 eos
 rm tmp-omp-json
