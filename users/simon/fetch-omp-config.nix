94a896f9c31afbeae76c1fb2a2c5257cd99b2d11
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-09-04 20:13:12 +0200

    update omp config

--- users/simon/fetch-omp-config.nix
+++ users/simon/fetch-omp-config.nix
@@ -1,4 +1,4 @@
 {
   url = "https://gist.githubusercontent.com/vincentmalloy/4b85151d28b3025451f5634b6081019d/raw/config.json";
-  sha256 = "1vm1y6zqh3vlwd2ba2b4p377zcsxn4ggx8m35bb2lxac5wym1yp0";
+  sha256 = "1m28pg5za7jsp2iq24ml76limzk95qrkz1xq00x6agy7cpyxw28z";
 }
