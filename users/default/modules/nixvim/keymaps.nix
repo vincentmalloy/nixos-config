034831da25313956ea4c9285b8697248724caeba
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-09-10 09:25:36 +0200

    update

---
+++ users/default/modules/nixvim/keymaps.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  programs.nixvim = {
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>m";
+        options.silent = true;
+        action = "<cmd>!make<CR>";
+      }
+    ];
+  };
+}
