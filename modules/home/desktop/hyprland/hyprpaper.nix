fd1a588da0c913d7fb9a80a8c9b36798f4482f88
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-09-15 14:20:05 +0200

    update

---
+++ modules/home/desktop/hyprland/hyprpaper.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+with lib; let
+  cfg = config.bundles.desktop.hyprland.hyprpaper;
+in {
+  options.bundles.desktop.hyprland.hyprpaper = {
+    enable = mkEnableOption "hyprpaper wallpaper utility";
+  };
+
+  config = mkIf cfg.enable {
+    services.hyprpaper = {
+      enable = true;
+      package = inputs.hyprpaper.packages.${pkgs.stdenv.hostPlatform.system}.hyprpaper;
+      settings = {
+        preload = [
+          "~/nixos-config/images/desktop/desktop_left.jpg"
+          "~/nixos-config/images/desktop/desktop_mid.jpg"
+          "~/nixos-config/images/desktop/desktop_right.jpg"
+        ];
+        wallpaper = [
+          "DP-1, ~/nixos-config/images/desktop/desktop_right.jpg"
+          "DP-2, ~/nixos-config/images/desktop/desktop_left.jpg"
+          "HDMI-A-1, ~/nixos-config/images/desktop/desktop_mid.jpg"
+        ];
+      };
+    };
+  };
+}
