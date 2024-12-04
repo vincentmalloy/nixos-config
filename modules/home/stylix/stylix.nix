7e316cee0d2b979147ce565955749cdf9c1a74a3
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-12-04 08:38:35 +0100

    update

--- modules/home/stylix/stylix.nix
+++ modules/home/stylix/stylix.nix
@@ -23,7 +23,7 @@ in {
         monospace = {
           # package = pkgs.nerdfonts.override {fonts = ["CommitMono"];};
           # will be in nerd-fonts namespace in nixos 25
-          package = pkgs.nerd-fonts.commit-mono;
+          package = if osConfig.settings.isWSL then pkgs.nerdfonts.override {fonts = ["CommitMono"];} else pkgs.nerd-fonts.commit-mono;
           name = "CommitMono Nerd Font";
         };
         serif = config.stylix.fonts.monospace;
