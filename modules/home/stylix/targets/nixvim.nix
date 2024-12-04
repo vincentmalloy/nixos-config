f4df15587de40de8a703ba4e57498a90a3e75372
Simon Lundius <25029432+vincentmalloy@users.noreply.github.com>
2024-12-04 08:32:19 +0100

    update

--- modules/home/stylix/targets/nixvim.nix
+++ modules/home/stylix/targets/nixvim.nix
@@ -8,12 +8,12 @@
 with lib; let
   cfg = osConfig.stylix;
 in {
-  config = mkIf (cfg.enable && !osConfig.settings.isWSL) {
-    stylix.targets.nixvim = {
-      transparentBackground = {
-        main = true;
-        signColumn = false;
-      };
-    };
+  config = mkIf (cfg.enable && (!osConfig.settings.isWSL)) {
+    #stylix.targets.nixvim = {
+    #  transparentBackground = {
+    #    main = true;
+    #    signColumn = false;
+    #  };
+    #};
   };
 }
